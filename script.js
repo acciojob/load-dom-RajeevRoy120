1c107ff4a3478f8168828dd8352006429037ef0c
Rajeev Roy <109815313+RajeevRoy120@users.noreply.github.com>
2025-03-20 09:57:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", () => {
+  // ✅ Create a new div element
+  const message = document.createElement("div");
+  message.innerText = "DOM load success";
+
+  // ✅ Append the div to the body
+  document.body.appendChild(message);
+});
