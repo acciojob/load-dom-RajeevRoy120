0205e6660d664e97058a8b0743e235b29da1f31b
Rajeev Roy <109815313+RajeevRoy120@users.noreply.github.com>
2025-03-20 10:06:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 //your JS code here. If required.
-document.addEventListener("DOMContentLoaded", () => {
-  // ✅ Create a new div element
-  const message = document.createElement("div");
-  message.innerText = "DOM load success";
+document.addEventListener("DOMContentLoaded", function() {
+  // Create a new div element
+  var newDiv = document.createElement("div"); 
 
-  // ✅ Append the div to the body
-  document.body.appendChild(message);
+  // Create a text node with the message
+  var newContent = document.createTextNode("DOM load success"); 
+
+  // Add the text node to the newly created div
+  newDiv.appendChild(newContent);  
+
+  // Add the newly created element and its content into the DOM 
+  var currentDiv = document.getElementById("div1"); 
+  document.body.insertBefore(newDiv, currentDiv); 
 });
